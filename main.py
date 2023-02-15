e816689f055333f01318605bdcf01c187f996287
Carmen de Lope Contreras <carmen@napptive.com>
2023-02-15 13:25:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import threading
 
 from tokenizers import Tokenizer
 from tokenizers.models import BPE
@@ -12,33 +13,37 @@ app = Flask(__name__)
 @app.route('/')
 def serve():
     return output
+
+def implementation():
+    output = "<p> tokenizers example </p>"
+    print("tokenizers example")
+    tokenizer = Tokenizer(BPE(unk_token="[UNK]"))
+    trainer = BpeTrainer(special_tokens=["[UNK]", "[CLS]", "[SEP]", "[PAD]", "[MASK]"])
+    tokenizer.pre_tokenizer = Whitespace()
+
+    output = "<p>Initilize files</p>"
+    print("Initilize files")
+    files = [f"./wikitext-103-raw/wiki.{split}.raw" for split in ["test", "train", "valid"]]
+
+    output = "<p>Init trainning</p>"
+    print("Trainning")
+    tokenizer.train(files, trainer)
+    output = "<p>Trainning finished</p>"
+    print("save the results")
+    tokenizer.save("./tokenizer-wiki.json")
+    output = tokenizer.encode("Hello, y'all! How are you 😁 ?")
+    print(output.tokens)
+
+    print("numpy example")
+    arr = np.array([1, 2, 3, 4, 5])
+
+    print(arr)
+    
+    output = "<p> Post processing value </p>"
+ 
+
+x = threading.Thread(target=implementation)
+x.start()
     
 app.run(host='0.0.0.0', port=5000)
 
-output = "<p> tokenizers example </p>"
-print("tokenizers example")
-tokenizer = Tokenizer(BPE(unk_token="[UNK]"))
-trainer = BpeTrainer(special_tokens=["[UNK]", "[CLS]", "[SEP]", "[PAD]", "[MASK]"])
-tokenizer.pre_tokenizer = Whitespace()
-
-output = "<p>Initilize files</p>"
-print("Initilize files")
-files = [f"./wikitext-103-raw/wiki.{split}.raw" for split in ["test", "train", "valid"]]
-
-output = "<p>Init trainning</p>"
-print("Trainning")
-tokenizer.train(files, trainer)
-output = "<p>Trainning finished</p>"
-print("save the results")
-tokenizer.save("./tokenizer-wiki.json")
-output = tokenizer.encode("Hello, y'all! How are you 😁 ?")
-print(output.tokens)
-
-print("numpy example")
-arr = np.array([1, 2, 3, 4, 5])
-
-print(arr)
-
-print("Example finished!")
-
-output = "<p> Post processing value </p>"
